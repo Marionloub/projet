8005f4d88d5f1bd64dfda36132256977ba2fe47f
MarionLoubiere <marion.loubiere@edu.ece.fr>
2026-03-19 14:16:35 +0100

    brancheu2

--- OneDrive/Documents/projetGit/projet1.py
+++ OneDrive/Documents/projetGit/projet1.py
@@ -8,3 +8,6 @@ class Gens :
 
     def se_presenter(self):
         print(f"Bonjour, je m'appelle {self.prenom} {self.nom}.")
+
+personne1 = Gens("Dupont", "Jean")
+personne1.se_presenter()              
